b19bc986f09e4e6c802a798a7f77ad730493051e
Talescruzs <tcs2017@hotmail.com>
2024-10-28 16:36:00 -0300

    parece que o quick está funcionando

--- bucketQuick.c
+++ bucketQuick.c
@@ -3,11 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct Node {
-  int data;
-  struct Node *next;
-};
-
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
@@ -40,53 +35,58 @@ int getBucketIndex(int value, int interval) {
   return value / interval;
 }
 
-void BucketSort(int arr[], int n, int nBucket, int interval) {
-  int i, j;
-  struct Node **buckets;
+void BucketSort(int arr[], int n, int n_bucket, int interval) {
+    int** buckets = (int**)malloc(n_bucket * sizeof(int*));
+    int* bucket_sizes = (int*)malloc(n_bucket * sizeof(int));
 
-  buckets = (struct Node **)malloc(sizeof(struct Node *) * nBucket);
+    int i, j;
 
-  for (i = 0; i < nBucket; ++i) {
-    buckets[i] = NULL;
-  }
-
-  for (i = 0; i < n; ++i) {
-    struct Node *current;
-    int pos = getBucketIndex(arr[i], interval);
-    current = (struct Node *)malloc(sizeof(struct Node));
-    current->data = arr[i];
-    current->next = buckets[pos];
-    buckets[pos] = current;
-  }
-
-  for (i = 0; i < nBucket; i++) {
-    printf("Bucket[%d]: ", i);
-    printBuckets(buckets[i]);
-    printf("\n");
-  }
-
-  for (i = 0; i < nBucket; ++i) {
-    buckets[i] = quick_sort(buckets[i], i*interval, ((i+1)*interval)-1);
-  }
-
-  printf("-------------\n");
-  printf("Bucktets after sorting\n");
-  for (i = 0; i < nBucket; i++) {
-    printf("Bucket[%d]: ", i);
-    printBuckets(buckets[i]);
-    printf("\n");
-  }
-
-  for (j = 0, i = 0; i < nBucket; ++i) {
-    struct Node *node;
-    node = buckets[i];
-    while (node) {
-      arr[j++] = node->data;
-      node = node->next;
+    for (i = 0; i < n_bucket; i++) {
+        buckets[i] = (int*)malloc(n * sizeof(int));
+        bucket_sizes[i] = 0;
     }
-  }
 
-  return;
+    for (i = 0; i < n; i++) {
+        int pos = arr[i] / interval;
+        if (pos >= n_bucket) {
+            pos = n_bucket - 1;
+        }
+        buckets[pos][bucket_sizes[pos]] = arr[i];
+        bucket_sizes[pos]++;
+    }
+
+    for (i = 0; i < n_bucket; i++) {
+        printf("Bucket[%d]: ", i);
+        for (j = 0; j < bucket_sizes[i]; j++) {
+            printf("%d ", buckets[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < n_bucket; i++) {
+        quick_sort(buckets[i], 0, bucket_sizes[i] - 1);
+    }
+
+    printf("-------------\n");
+    printf("Bucktets after sorting\n");
+    for (i = 0; i < n_bucket; i++) {
+        printf("Bucket[%d]: ", i);
+        for (j = 0; j < bucket_sizes[i]; j++) {
+            printf("%d ", buckets[i][j]);
+        }
+        printf("\n");
+    }
+
+    int k = 0;
+    for (i = 0; i < n_bucket; i++) {
+        for (j = 0; j < bucket_sizes[i]; j++) {
+            arr[k++] = buckets[i][j];
+        }
+        free(buckets[i]);
+    }
+
+    free(buckets);
+    free(bucket_sizes);
 }
 
 
